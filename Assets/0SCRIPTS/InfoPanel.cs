0b2b13cf230a0120e7869a36efcde25bf5f2b54c
pertsaboii <pertti.s.hakala@gmail.com>
2022-12-12 21:00:16 +0200

    almost done, global leaderboard needs fixes

--- Assets/0SCRIPTS/InfoPanel.cs
+++ Assets/0SCRIPTS/InfoPanel.cs
@@ -15,6 +15,7 @@ public class InfoPanel : MonoBehaviour
     public void InfoPanelOn(string name, string text, VideoClip clip)
     {
         SoundManager.Instance.PlayUISound(gamemanager.assetBank.FindSound(AssetBank.Sound.ButtonClicked));
+        SoundManager.Instance.PlayUISound(gamemanager.assetBank.FindSound(AssetBank.Sound.UISwoosh0));
         gameObject.transform.DOScale(Vector3.one, .3f).SetEase(Ease.OutSine);
         cardName.text = name;
         descriptionText.text = text;
@@ -23,6 +24,7 @@ public class InfoPanel : MonoBehaviour
     }
     public void InfoPanelOff()
     {
+        SoundManager.Instance.PlayUISound(gamemanager.assetBank.FindSound(AssetBank.Sound.UISwoosh0));
         videoPlayer.Stop();
         gameObject.transform.DOScale(Vector3.zero, .3f).SetEase(Ease.OutSine);
     }
